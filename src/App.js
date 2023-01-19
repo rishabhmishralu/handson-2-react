a0551db2de15293057f0771239753488a7ccbbe0
= <=>
2023-01-19 17:25:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css"
+export default class App extends Component {
+  state = {
+    Name: "",
+    Department: "",
+    Rating: "",
+    employees: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  chageHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submitHander = (e) => {
+    e.preventDefault()
+    const empObj = {
+      Name: this.state.Name,
+      Department: this.state.Department,
+      Rating: this.state.Rating
+    };
+    const arr = this.state.employees;
+    arr.push(empObj);
+    this.setState({ employees:arr });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="heading">EMPLOYEE FEEDBACK FORM</h1>
+        <br/>
+        <form className="form">
+          <label className="label">Name : </label>
+          <input className="input"
+            type="text"
+            name="Name"
+            value={this.state.Name}
+            onChange={this.chageHandler}
+          ></input>
+
+          <br></br>
+          <br></br>
+          <label className="label" >Department : </label>
+         
+          
+          <input className="input"
+            type="text"
+            name="Department"
+            value={this.state.Department}
+            onChange={this.chageHandler}
+          ></input>
+          <br></br>
+          <br></br>
+          <label className="label">Rating : </label>
+          <input className="input"
+            type="number"
+            name="Rating"
+            value={this.state.Rating}
+            onChange={this.chageHandler}
+          ></input>
+          <br></br>
+          <br></br>
+          <br/>
+          <button className="button" onClick={this.submitHander}>submit</button>
+        </form>
+    <br/><br/>
+        <div className="storedata">
+          {this.state.employees.map((value, index) => {
+            return <div>Name:{value.Name} Department:{value.Department} Rating:{value.Rating}       ||    </div>;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
